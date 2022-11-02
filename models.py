6458afe3ad02e511182e6564c8e68e40732100c9
aayush158 <shuklaayush577@gmail.com>
2022-11-02 23:41:52 +0530

    Initial commit

---
+++ models.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, session, redirect
+from flask_sqlalchemy import SQLAlchemy
+import json
+import math
+
+db = SQLAlchemy()
+
+class Contact(db.Model):
+
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+
+
+class Posts(db.Model):
+
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    subtitle = db.Column(db.String(80), nullable=False)
+    slug = db.Column(db.String(21), nullable=False)
+    content = db.Column(db.String(120), nullable=False)
+    img_file = db.Column(db.String(20), nullable=True)
+    date = db.Column(db.String(12), nullable=True)
+
+#db.create_all()
+
